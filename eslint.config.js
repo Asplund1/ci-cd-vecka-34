f94b696dc4fa64901f06673272c1c5d96d6d89b7
Asplund-chasacademy <andreas.asplund@chasacademy.se>
2025-08-25 16:43:43 +0200

    Tried going back and change, to see if that will solve the problems

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+// eslint.config.js
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: { ...globals.browser, ...globals.node }
+    },
+    rules: {
+      'no-unused-vars': 'error',
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'always']
+    }
+  }
+];
---
+++ frontend-backup/frontend/eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      semi: 'error',        // exempelregel: kräver semikolon
+      quotes: ['error', 'double'] // exempelregel: kräver dubbelfnuttar
+    }
+  }
+];
